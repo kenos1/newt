517bc1a374d0bbb5295170a6da0f8df747e78186
Kenos <75338427+kenos1@users.noreply.github.com>
2024-01-04 17:27:08 -0700

    typescript stupid

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,18 +5,23 @@ export default function Item(props: { item: SearchItem; class?: string }) {
   return (
     <Switch>
       <Match when={props.item.action === ItemActions.Link}>
+        {/* @ts-expect-error Typescript is dumb and doesn't understand context */}
         <a class={props.class} href={props.item.url}>
           <img
-            src={`https://icons.duckduckgo.com/ip3/${props.item.url.replace(
-              "https://",
-              "",
-            )}.ico`}
+            src={
+              // @ts-expect-error Typescript is dumb and doesn't understand context
+              `https://icons.duckduckgo.com/ip3/${props.item.url.replace(
+                "https://",
+                "",
+              )}.ico`
+            }
             class="icon"
           />
           {props.item.name}
         </a>
       </Match>
       <Match when={props.item.action === ItemActions.Setting}>
+        {/* @ts-expect-error Typescript is dumb and doesn't understand context */}
         <button class={props.class} onClick={() => props.item.callback()}>
           {props.item.name}
         </button>
